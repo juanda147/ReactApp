0a7c6ba39c9166cfc27bbefa7b577a56816a24b7
juandavid147@hotmail.com <juandavid147@hotmail.com>
2019-04-24 16:28:06 -0500

    partial

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -9,12 +9,10 @@ function App() {
   return (
     <div className="ui container comments" >
 
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
+      <CommentDetail author="Sam" timeAgo="Today at 4:45PM" avatar={faker.image.avatar()} comment="Hello"/>
+      <CommentDetail author="Alex" timeAgo="Today at 2:00AM" avatar={faker.image.avatar()} comment="World"/>
+      <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM"  avatar={faker.image.avatar()} comment="AAAA"/>
+
     </div>
 
 
