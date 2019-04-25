281d156a5c872b3b5ac317ef316675cda624cfcf
Juan David Castaneda <juandavid147@hotmail.com>
2019-04-24 23:02:47 -0500

    partial to section 4

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,15 +3,49 @@ import logo from './logo.svg';
 import './App.css';
 import faker from 'faker'
 import CommentDetail from './CommentDetail'
+import ApprovalCard from './ApprovalCard'
 
 
 function App() {
   return (
     <div className="ui container comments" >
 
-      <CommentDetail author="Sam" timeAgo="Today at 4:45PM" avatar={faker.image.avatar()} comment="Hello"/>
-      <CommentDetail author="Alex" timeAgo="Today at 2:00AM" avatar={faker.image.avatar()} comment="World"/>
-      <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM"  avatar={faker.image.avatar()} comment="AAAA"/>
+      <ApprovalCard>
+        <div>
+          <h4>
+            Are you sure you want to do this?
+          </h4>
+        </div>
+
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          avatar={faker.image.avatar()}
+          comment="Hello"
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:00AM"
+          avatar={faker.image.avatar()}
+          comment="World"
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 5:00PM"
+          avatar={faker.image.avatar()}
+          comment="AAAA"
+        />
+      </ApprovalCard>
+
 
     </div>
 
