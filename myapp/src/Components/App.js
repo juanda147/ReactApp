6d47616e449a62abbf9631c31cd45451533621b8
juandavid147@hotmail.com <juandavid147@hotmail.com>
2019-05-08 16:38:18 -0500

    partial up to lesson 124

--- myapp/src/Components/App.js
+++ myapp/src/Components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../api/youtube';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 
 class App extends React.Component {
 
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
 
   onTermSubmit = async (term) => {
     const response = await youtube.get('/search', {
@@ -16,12 +18,19 @@ class App extends React.Component {
 
     this.setState({ videos: response.data.items });
   };
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video })
+  };
 
   render() {
     return (
       <div className="container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        I have {this.state.videos.length} videos
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList 
+            onVideoSelect={this.onVideoSelect} 
+            videos={this.state.videos} 
+        />
       </div>
     );
   };
