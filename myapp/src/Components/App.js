e9a0ee25d1d187966f4424efe91a0d206e503c17
juandavid147@hotmail.com <juandavid147@hotmail.com>
2019-05-09 16:08:51 -0500

    Re-structured components folder

--- myapp/src/Components/App.js
+++ myapp/src/Components/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../api/youtube';
-import VideoList from './VideoList';
-import VideoDetail from './VideoDetail';
+import VideoList from './youtube/VideoList';
+import VideoDetail from './youtube/VideoDetail';
 
 
 class App extends React.Component {
